e556f72788b6eb31349fbc0aa0373ae56f498c94
Manthan Dave <manthanhd@live.com>
2016-03-20 10:08:11 +0000

    Javadoc added.

--- src/main/java/com/manthanhd/utils/superstringreplace/SuperString.java
+++ src/main/java/com/manthanhd/utils/superstringreplace/SuperString.java
@@ -6,17 +6,28 @@ import java.util.Stack;
 /**
  * Created by manthanhd on 18/03/2016.
  */
+
+/***
+ * Super String base class.
+ */
 public class SuperString {
     public static final char DELIM = ':';
 
     private final String originalString;
     private final Stack<Marker> markerStack = new Stack<>();
 
+    /***
+     * Main constructor. Tokens are immediately generated.
+     * @param originalString Immutable string object reference. Original string is not modified.
+     */
     public SuperString(final String originalString) {
         this.originalString = originalString;
         generateTokens();
     }
 
+    /***
+     * Generates and caches tokens.
+     */
     private void generateTokens() {
         StringBuilder markerBuilder = new StringBuilder();
         boolean recording = false;
@@ -42,6 +53,11 @@ public class SuperString {
         }
     }
 
+    /***
+     * Replace labels in the original string with values provided in the parameter.
+     * @param replaceValues Map with label name as key and value to replace as its value.
+     * @return New string object with labels replaced with values provided. Labels whose values weren't provided are left untouched.
+     */
     public String replace(Map<String, String> replaceValues) {
         final StringBuilder newString = new StringBuilder(originalString);
         for (int i = markerStack.size() - 1; i >= 0; i--) {
@@ -54,6 +70,9 @@ public class SuperString {
         return newString.toString();
     }
 
+    /***
+     * Private class to Super String for marking tokens.
+     */
     private static class Marker {
         private int index;
         private String marker;
